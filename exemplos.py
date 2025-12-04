15d0cabbbeb50bda3bdfd8ea157604ac7878c6b5
Bequendiohans <asafesias@gmail.com>
2025-12-04 19:28:11 -0300

    Introdução a lista

---
+++ exemplos.py
@@ -0,0 +1,12 @@
+#Aula sobre Listas
+#São variáveis compostas para armazenament de mais informações por vez
+#É caracterizado o Python pelo []
+#Por convenção, usamos o nome das listas no plural. É opcional o prefixo "list", "lst"
+
+list_meses = ["janeiro", "fevereiro", "março", "abril", "maio","junho"]
+
+print(list_meses[0])  #chamar pelo indice
+print(list_meses[-1])  #chama o último elemento
+print(list_meses[1:4])  #Imprime a partir do índice 1 até o 4-1 (Não vai até o final)
+print(list_meses[2:])  #Imprime a partir do índice 2 até o final.
+print(list_meses[:4])  #Imprime a partir do índice 0 até o 5-1
